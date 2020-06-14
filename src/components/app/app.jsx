9b6492e56c378a0bd6271fe56becaa803978d9a4
Georg <georgiy.nikitin@gmail.com>
2020-06-14 10:43:33 +0300

    Создан снепшот-тест на  src/components/app/app.test.js

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,25 +1,16 @@
 import Main from "../main/main.jsx";
 import React from "react";
-import PropTypes from "prop-types";
 
 const App = (props) => {
-
-  const {filmsTitles, genreFilm, titleFilm, releaseDate} = props;
+  // eslint-disable-next-line react/prop-types
+  const {genreFilm, titleFilm, releaseDate} = props;
   return (
     <Main
-      filmsTitles={filmsTitles}
       genreFilm={genreFilm}
       titleFilm={titleFilm}
       releaseDate={releaseDate}
     />
   );
-};
-
-App.propTypes = {
-  filmsTitles: PropTypes.array.isRequired,
-  genreFilm: PropTypes.string.isRequired,
-  titleFilm: PropTypes.string.isRequired,
-  releaseDate: PropTypes.number.isRequired
 };
 
 export default App;
