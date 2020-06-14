9b3cbad5ad082ff0c44ba14582e79dde92268855
Georg <georgiy.nikitin@gmail.com>
2020-06-14 12:43:44 +0300

    Добавит в пропс массив названий фильмов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,9 +3,10 @@ import React from "react";
 
 const App = (props) => {
   // eslint-disable-next-line react/prop-types
-  const {genreFilm, titleFilm, releaseDate} = props;
+  const {filmsTitles, genreFilm, titleFilm, releaseDate} = props;
   return (
     <Main
+      filmsTitles={filmsTitles}
       genreFilm={genreFilm}
       titleFilm={titleFilm}
       releaseDate={releaseDate}
