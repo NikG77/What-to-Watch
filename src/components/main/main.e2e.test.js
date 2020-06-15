0c47c956a0ac29f6864da5a603109a7bacc5aaf8
Georg <georgiy.nikitin@gmail.com>
2020-06-15 12:01:36 +0300

    Fix клика на все ссылки фильмов

--- src/components/main/main.e2e.test.js
+++ src/components/main/main.e2e.test.js
@@ -3,6 +3,11 @@ import Enzyme, {mount} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import Main from "./main";
 
+const filmsTitles = [`Fantastic Beasts: The Crimes of Grindelwald`, `Bohemian Rhapsody`, `Macbeth Aviator`, `We need to talk about Kevin`, `What We Do in the Shadows Revenant`, `Johnny English`, `Shutter Island`, `Pulp Fiction`, `No Country for Old Men`, `Snatch`, `Moonrise Kingdom`, `Seven Years in Tibet`, `Midnight Special`, `War of the Worlds`, `Dardjeeling Limited`, `Orlando, Mindhunter`, `Midnight Special`];
+const genreFilm = `Drama`;
+const titleFilm = `The Grand Budapest Hotel`;
+const releaseDate = 2014;
+
 Enzyme.configure({
   adapter: new Adapter(),
 });
@@ -13,19 +18,19 @@ describe(`MainComponent`, () => {
 
     const main = mount(
         <Main
-          filmsTitles={[`Fantastic Beasts: The Crimes of Grindelwald`, `Bohemian Rhapsody`, `Macbeth Aviator`, `We need to talk about Kevin`, `What We Do in the Shadows Revenant`, `Johnny English`, `Shutter Island`, `Pulp Fiction`, `No Country for Old Men`, `Snatch`, `Moonrise Kingdom`, `Seven Years in Tibet`, `Midnight Special`, `War of the Worlds`, `Dardjeeling Limited`, `Orlando, Mindhunter`, `Midnight Special`]}
-          genreFilm={`Drama`}
-          titleFilm={`The Grand Budapest Hotel`}
-          releaseDate={2014}
+          filmsTitles={filmsTitles}
+          genreFilm={genreFilm}
+          titleFilm={titleFilm}
+          releaseDate={releaseDate}
           onSmallMovieCardClick={onSmallMovieCardClick}
         />
     );
 
-    const movieCardLink = main.find(`a.small-movie-card__link`);
+    const movieCardLinks = main.find(`a.small-movie-card__link`);
     // movieCardLink.props().onClick();
-    movieCardLink.simulate(`click`, {preventDefault() {}});
+    movieCardLinks.forEach((movieCardLink) => movieCardLink.simulate(`click`, {preventDefault() {}}));
 
-    expect(onSmallMovieCardClick.mock.calls.length).toBe(1);
+    expect(onSmallMovieCardClick.mock.calls.length).toBe(filmsTitles.length);
   });
 
 });
