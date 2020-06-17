b12252c3703dad5491da2dfcbbc54c8a724c9937
Georg <georgiy.nikitin@gmail.com>
2020-06-17 19:17:35 +0300

    Подключит компоненты в main.jsx

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import MoviesList from "../movies-list/movies-list.jsx";
 
 const Main = (props) => {
 
@@ -98,18 +99,10 @@ const Main = (props) => {
             </li>
           </ul>
 
-          <div className="catalog__movies-list">
-            {filmsTitles.map((title, i) =>
-              <article key={title + i} className="small-movie-card catalog__movies-card">
-                <div className="small-movie-card__image">
-                  <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg" alt={title} width="280" height="175" />
-                </div>
-                <h3 onClick={onSmallMovieCardClick} className="small-movie-card__title">
-                  <a className="small-movie-card__link" href="movie-page.html">{title}</a>
-                </h3>
-              </article>
-            )}
-          </div>
+          <MoviesList
+            filmsTitles={filmsTitles}
+            onSmallMovieCardClick={onSmallMovieCardClick}
+          />
 
           <div className="catalog__more">
             <button className="catalog__button" type="button">Show more</button>
