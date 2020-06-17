e4f04814bd4e37d1068722084ff9bf834911c036
Georg <georgiy.nikitin@gmail.com>
2020-06-17 15:17:42 +0300

    Добавлена структура фильма для small card

--- src/mock/films.js
+++ src/mock/films.js
@@ -1,2 +1,11 @@
 export const filmsTitles = [`Fantastic Beasts: The Crimes of Grindelwald`, `Bohemian Rhapsody`, `Macbeth Aviator`, `We need to talk about Kevin`, `What We Do in the Shadows Revenant`, `Johnny English`, `Shutter Island`, `Pulp Fiction`, `No Country for Old Men`, `Snatch`, `Moonrise Kingdom`, `Seven Years in Tibet`, `Midnight Special`, `War of the Worlds`, `Dardjeeling Limited`, `Orlando, Mindhunter`, `Midnight Special`];
 
+export const films = [{title: `Fantastic Beasts: The Crimes of Grindelwald`, src: `img/bohemian-rhapsody.jpg`},
+  {title: `Bohemian Rhapsody`, src: `img/bohemian-rhapsody.jpg`},
+  {title: `Macbeth Aviator`, src: `img/bohemian-rhapsody.jpg`},
+  {title: `We need to talk about Kevin`, src: `img/bohemian-rhapsody.jpg`},
+  {title: `What We Do in the Shadows Revenant`, src: `img/bohemian-rhapsody.jpg`},
+  {title: `Johnny English`, src: `img/bohemian-rhapsody.jpg`},
+  {title: `Pulp Fiction`, src: `img/bohemian-rhapsody.jpg`},
+  {title: `No Country for Old Men`, src: `img/bohemian-rhapsody.jpg`}];
+
