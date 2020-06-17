02cb134964ddb079c1c75cd7fe94dcecfd0f943d
Georg <georgiy.nikitin@gmail.com>
2020-06-17 22:07:34 +0300

    Добавляет обработчик на hover

--- src/components/small-movie-card/small-movie-card.jsx
+++ src/components/small-movie-card/small-movie-card.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const SmallMovieCard = (props) => {
-  const {title, onSmallMovieCardClick} = props;
+  const {title, onSmallMovieCardClick, onSmallMovieCardHover} = props;
 
   return (
     <article className="small-movie-card catalog__movies-card">
       <div className="small-movie-card__image">
         <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg" alt={title} width="280" height="175" />
       </div>
-      <h3 onClick={onSmallMovieCardClick} className="small-movie-card__title">
+      <h3 onClick={onSmallMovieCardClick}
+        onMouseEnter={() => {
+          onSmallMovieCardHover(title);
+        }}
+        className="small-movie-card__title">
         <a className="small-movie-card__link" href="movie-page.html">{title}</a>
       </h3>
     </article>
@@ -21,6 +25,7 @@ SmallMovieCard.propTypes = {
   title: PropTypes.string.isRequired,
 
   onSmallMovieCardClick: PropTypes.func.isRequired,
+  onSmallMovieCardHover: PropTypes.func.isRequired,
 };
 
 export default SmallMovieCard;
