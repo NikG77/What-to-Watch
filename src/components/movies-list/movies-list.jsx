6ebd838c59dd83337f02c55afe28216b8173ad85
Georg <georgiy.nikitin@gmail.com>
2020-06-18 14:39:11 +0300

    Добавлет в setState изменение объекта

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -5,17 +5,18 @@ import SmallMovieCard from "../small-movie-card/small-movie-card.jsx";
 class MoviesList extends PureComponent {
   constructor(props) {
     super(props);
-    this.state = null;
+    this.state = {film: null};
 
     this.handleSmallMovieCardHover = this.handleSmallMovieCardHover.bind(this);
   }
 
   handleSmallMovieCardHover(film) {
-    this.setState = film;
+    this.setState({film});
   }
 
   render() {
     const {films, onSmallMovieCardClick} = this.props;
+
 
     return (
       <div className="catalog__movies-list">
