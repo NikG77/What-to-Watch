64fa64106ce686f97434c58d4f145a2e43068633
Georg <georgiy.nikitin@gmail.com>
2020-06-18 10:17:45 +0300

    Добавил проверку на hover

--- src/components/small-movie-card/small-movie-card.e2e.test.js
+++ src/components/small-movie-card/small-movie-card.e2e.test.js
@@ -12,7 +12,7 @@ Enzyme.configure({
 });
 
 
-it(`Should small movie card link be pressed`, () => {
+it(`Should small movie card be pressed`, () => {
   const onSmallMovieCardClick = jest.fn();
   const onSmallMovieCardHover = jest.fn();
 
@@ -27,6 +27,25 @@ it(`Should small movie card link be pressed`, () => {
   const movieCardLinks = smallMovieCard.find(`.small-movie-card__title`);
 
   movieCardLinks.simulate(`click`);
+
+  expect(onSmallMovieCardClick.mock.calls.length).toBe(1);
+});
+
+it(`Should small movie card be hovered`, () => {
+  const onSmallMovieCardClick = jest.fn();
+  const onSmallMovieCardHover = jest.fn();
+
+  const smallMovieCard = shallow(
+      <SmallMovieCard
+        film={film}
+        onSmallMovieCardHover={onSmallMovieCardHover}
+        onSmallMovieCardClick={onSmallMovieCardClick}
+      />
+  );
+
+  const movieCardLinks = smallMovieCard.find(`.small-movie-card__title`);
+
+  movieCardLinks.simulate(`mouseover`);
 
   expect(onSmallMovieCardClick.mock.calls.length).toBe(1);
 });
