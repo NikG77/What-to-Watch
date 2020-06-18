9528c6bcfdeae5f7f2921b7dcc8fb2fb4f565877
Georg <georgiy.nikitin@gmail.com>
2020-06-18 17:38:45 +0300

    Измненил app.jsx в класс компонент и подключил BrowserRouter

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,22 +1,54 @@
 import Main from "../main/main.jsx";
-import React from "react";
+import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 
-const smallMovieCardHandler = () => {};
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
 
-const App = (props) => {
+  _renderApp() {
+    const smallMovieCardHandler = () => {};
+    const {films, genreFilm, titleFilm, releaseDate} = this.props;
+    return (
+      <Main
+        films={films}
+        genreFilm={genreFilm}
+        titleFilm={titleFilm}
+        releaseDate={releaseDate}
+        onSmallMovieCardClick={smallMovieCardHandler}
+      />
+    );
 
-  const {films, genreFilm, titleFilm, releaseDate} = props;
-  return (
-    <Main
-      films={films}
-      genreFilm={genreFilm}
-      titleFilm={titleFilm}
-      releaseDate={releaseDate}
-      onSmallMovieCardClick={smallMovieCardHandler}
-    />
-  );
-};
+  }
+
+  render() {
+    const smallMovieCardHandler = () => {};
+    const {films, genreFilm, titleFilm, releaseDate} = this.props;
+
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            {this._renderApp()}
+          </Route>
+          <Route exact path="/dev">
+            <Main
+              films={films}
+              genreFilm={genreFilm}
+              titleFilm={titleFilm}
+              releaseDate={releaseDate}
+              onSmallMovieCardClick={smallMovieCardHandler}
+            />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+
+    );
+  }
+
+}
 
 App.propTypes = {
   films: PropTypes.arrayOf(PropTypes.shape({
