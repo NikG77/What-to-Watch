7147fdf0924eb87cdce93bb092fedef35a0b58b9
Georg <georgiy.nikitin@gmail.com>
2020-06-19 11:10:42 +0300

    Добавит функцию расчета уровня рейтинга по оценке и корректное перечисление актеров

--- src/components/movie-page/movie-page.jsx
+++ src/components/movie-page/movie-page.jsx
@@ -1,11 +1,37 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {RatingLevel} from "../../const.js";
 
 
 const MoviePage = (props) => {
   const {film} = props;
   const {title, ratingScore, ratingCount, director, starring, genre, releaseDate, poster, pictureBackground} = film;
-  // const {genre, releaseDate, poster, pictureBackground} = mainFilm;
+  const starringDetails = starring.join(`, `);
+
+  const getRatingLevel = (score) => {
+    let ratingLevel = ``;
+
+    switch (true) {
+      case score < RatingLevel.BAD_MAX && score >= RatingLevel.BAD_MIN:
+        ratingLevel = `Bad`;
+        break;
+      case score < RatingLevel.NORMAL_MAX && score >= RatingLevel.NORMAL_MIN:
+        ratingLevel = `Normal`;
+        break;
+      case score < RatingLevel.COOD_MAX && score >= RatingLevel.COOD_MIN:
+        ratingLevel = `Good`;
+        break;
+      case score < RatingLevel.VERY_GOOD_MAX && score >= RatingLevel.VERY_GOOD_MIN:
+        ratingLevel = `Very good`;
+        break;
+      case score === RatingLevel.AWESOME:
+        ratingLevel = `Awesome`;
+        break;
+    }
+    return ratingLevel;
+  };
+
+  const ratingLevel = getRatingLevel(ratingScore);
 
   return (
     <section className="movie-card movie-card--full">
@@ -83,19 +109,19 @@ const MoviePage = (props) => {
             <div className="movie-rating">
               <div className="movie-rating__score">{ratingScore}</div>
               <p className="movie-rating__meta">
-                <span className="movie-rating__level">Very good</span>
+                <span className="movie-rating__level">{ratingLevel}</span>
                 <span className="movie-rating__count">{ratingCount} ratings</span>
               </p>
             </div>
 
             <div className="movie-card__text">
-              <p>In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave&lsquo s friend and protege.</p>
+              <p>In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave&lsquo;s friend and protege.</p>
 
-              <p>Gustave prides himself on providing first-class service to the hotel&apos s guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave&lsquo s lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.</p>
+              <p>Gustave prides himself on providing first-class service to the hotel&apos;s guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave&lsquo;s lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.</p>
 
               <p className="movie-card__director"><strong>Director: {director}</strong></p>
 
-              <p className="movie-card__starring"><strong>Starring: {starring} and other</strong></p>
+              <p className="movie-card__starring"><strong>Starring: {starringDetails} and other</strong></p>
             </div>
           </div>
         </div>
