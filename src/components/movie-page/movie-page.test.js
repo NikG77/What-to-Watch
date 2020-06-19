f93367b3105faf78458cae721f1e518681f2e80c
Georg <georgiy.nikitin@gmail.com>
2020-06-19 15:19:02 +0300

    Добавляет снепшот-тест на компонент movie-page.jsx

---
+++ src/components/movie-page/movie-page.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import MoviePage from "./movie-page.jsx";
+
+const film = {
+  title: `Fantastic Beasts: The Crimes of Grindelwald`,
+  src: `img/bohemian-rhapsody.jpg`,
+  poster: ``,
+  ratingScore: 6.7,
+  ratingCount: 200,
+  director: ``,
+  starring: [`Robert De Niro`, `Matt Damon`, `Tom Hanks`],
+  genre: ``,
+  releaseDate: 2000,
+  pictureBackground: ``,
+};
+
+
+it(`Should MoviePage render correctly`, () => {
+  const tree = renderer
+    .create(<MoviePage
+      film={film}
+    />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
