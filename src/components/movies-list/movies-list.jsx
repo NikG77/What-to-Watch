06d6195ce697036c4345c14091b273e630ba5d06
Georg <georgiy.nikitin@gmail.com>
2020-07-03 12:02:51 +0300

    Переделывает компонент MoviesList в функциональный компонент

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -1,29 +1,28 @@
-import React, {PureComponent} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import SmallMovieCard from "../small-movie-card/small-movie-card.jsx";
 import {filmsType, filmType} from "../../types";
 
 
-class MoviesList extends PureComponent {
+const MoviesList = (props) => {
 
-  render() {
-    const {films, onSmallMovieCardClick, activeItem, onItemClick} = this.props;
+  const {films, onSmallMovieCardClick, activeItem, onItemClick} = props;
 
-    return (
-      <div className="catalog__movies-list">
-        {films.map((movie, i) =>
-          <SmallMovieCard
-            key={movie + i}
-            film={movie}
-            onSmallMovieCardClick={onSmallMovieCardClick}
-            onSmallMovieCardHover={onItemClick}
-            isPlaying={activeItem === movie}
-          />
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="catalog__movies-list">
+      {films.map((movie, i) =>
+        <SmallMovieCard
+          key={movie + i}
+          film={movie}
+          onSmallMovieCardClick={onSmallMovieCardClick}
+          onSmallMovieCardHover={onItemClick}
+          isPlaying={activeItem === movie}
+        />
+      )}
+    </div>
+  );
+
+};
 
 MoviesList.propTypes = {
   films: filmsType.isRequired,
