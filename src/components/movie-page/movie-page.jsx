e3b35cbf77e252c17b601ae2441b0c1d15e8917d
Georg <georgiy.nikitin@gmail.com>
2020-07-04 17:37:11 +0300

    Реализует функционал «More like this» с помощью существующего компонента MoviesList

--- src/components/movie-page/movie-page.jsx
+++ src/components/movie-page/movie-page.jsx
@@ -1,94 +1,128 @@
 import React from "react";
-// import {RatingLevel} from "../../const.js";
-import {filmType} from "../../types";
+import {filmType, filmsType} from "../../types";
 import PropTypes from "prop-types";
 import Tabs from "../tabs/tabs.jsx";
 import withActiveItem from "../../hocs/with-active-item/with-active-item.js";
+import MoviesList from "../movies-list/movies-list.jsx";
+
 
 const TabsWrapped = withActiveItem(Tabs);
+const MoviesListWrapped = withActiveItem(MoviesList);
 
 const MoviePage = (props) => {
-  const {film} = props;
+  const {film, films, onSmallMovieCardClick} = props;
   const {title, genre, releaseDate, poster, pictureBackground} = film;
+  const likeFilms = films.filter((movie) => {
+    return movie !== film;
+  });
+  likeFilms.splice(4);
 
   return (
-    <section className="movie-card movie-card--full">
-      <div className="movie-card__hero">
-        <div className="movie-card__bg">
-          <img src={pictureBackground} alt={title} />
+    <React.Fragment>
+      <section className="movie-card movie-card--full">
+        <div className="movie-card__hero">
+          <div className="movie-card__bg">
+            <img src={pictureBackground} alt={title} />
+          </div>
+
+          <h1 className="visually-hidden">WTW</h1>
+
+          <header className="page-header movie-card__head">
+            <div className="logo">
+              <a href="main.html" className="logo__link">
+                <span className="logo__letter logo__letter--1">W</span>
+                <span className="logo__letter logo__letter--2">T</span>
+                <span className="logo__letter logo__letter--3">W</span>
+              </a>
+            </div>
+
+            <div className="user-block">
+              <div className="user-block__avatar">
+                <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
+              </div>
+            </div>
+          </header>
+
+          <div className="movie-card__wrap">
+            <div className="movie-card__desc">
+              <h2 className="movie-card__title">{title}</h2>
+              <p className="movie-card__meta">
+                <span className="movie-card__genre">{genre}</span>
+                <span className="movie-card__year">{releaseDate}</span>
+              </p>
+
+              <div className="movie-card__buttons">
+                <button className="btn btn--play movie-card__button" type="button">
+                  <svg viewBox="0 0 19 19" width="19" height="19">
+                    <use xlinkHref="#play-s"></use>
+                  </svg>
+                  <span>Play</span>
+                </button>
+                <button className="btn btn--list movie-card__button" type="button">
+                  <svg viewBox="0 0 19 20" width="19" height="20">
+                    <use xlinkHref="#add"></use>
+                  </svg>
+                  <span>My list</span>
+                </button>
+                <a href="add-review.html" className="btn movie-card__button">Add review</a>
+              </div>
+            </div>
+          </div>
         </div>
 
-        <h1 className="visually-hidden">WTW</h1>
+        <div className="movie-card__wrap movie-card__translate-top">
+          <div className="movie-card__info">
+            <div className="movie-card__poster movie-card__poster--big">
+              <img src={poster} alt={title} width="218" height="327" />
+            </div>
 
-        <header className="page-header movie-card__head">
+            <div className="movie-card__desc">
+
+              <TabsWrapped
+                film={film}
+              />
+
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className="page-content">
+
+        <section className="catalog catalog--like-this">
+          <h2 className="catalog__title">More like this</h2>
+          {likeFilms.length > 0 ? <MoviesListWrapped films={likeFilms} onSmallMovieCardClick={onSmallMovieCardClick} /> : ``}
+
+
+        </section>
+
+
+        <footer className="page-footer">
           <div className="logo">
-            <a href="main.html" className="logo__link">
+            <a href="main.html" className="logo__link logo__link--light">
               <span className="logo__letter logo__letter--1">W</span>
               <span className="logo__letter logo__letter--2">T</span>
               <span className="logo__letter logo__letter--3">W</span>
             </a>
           </div>
 
-          <div className="user-block">
-            <div className="user-block__avatar">
-              <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
-            </div>
+          <div className="copyright">
+            <p>© 2019 What to watch Ltd.</p>
           </div>
-        </header>
-
-        <div className="movie-card__wrap">
-          <div className="movie-card__desc">
-            <h2 className="movie-card__title">{title}</h2>
-            <p className="movie-card__meta">
-              <span className="movie-card__genre">{genre}</span>
-              <span className="movie-card__year">{releaseDate}</span>
-            </p>
-
-            <div className="movie-card__buttons">
-              <button className="btn btn--play movie-card__button" type="button">
-                <svg viewBox="0 0 19 19" width="19" height="19">
-                  <use xlinkHref="#play-s"></use>
-                </svg>
-                <span>Play</span>
-              </button>
-              <button className="btn btn--list movie-card__button" type="button">
-                <svg viewBox="0 0 19 20" width="19" height="20">
-                  <use xlinkHref="#add"></use>
-                </svg>
-                <span>My list</span>
-              </button>
-              <a href="add-review.html" className="btn movie-card__button">Add review</a>
-            </div>
-          </div>
-        </div>
+        </footer>
       </div>
-
-      <div className="movie-card__wrap movie-card__translate-top">
-        <div className="movie-card__info">
-          <div className="movie-card__poster movie-card__poster--big">
-            <img src={poster} alt={title} width="218" height="327" />
-          </div>
-
-          <div className="movie-card__desc">
-
-            <TabsWrapped
-              film={film}
-            />
-
-          </div>
-        </div>
-      </div>
-    </section>
+    </React.Fragment>
 
   );
 };
 
 MoviePage.propTypes = {
+  films: filmsType.isRequired,
   film: PropTypes.oneOfType([
     filmType.isRequired,
     PropTypes.oneOf([null]).isRequired,
   ]),
+  onSmallMovieCardClick: PropTypes.func.isRequired,
 };
-
 
 export default MoviePage;
