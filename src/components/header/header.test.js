5dccda6291ba7845eb17c3595c59dedd9b327036
Georg <georgiy.nikitin@gmail.com>
2020-07-17 13:21:37 +0300

    Рефакторинг

--- src/components/header/header.test.js
+++ src/components/header/header.test.js
@@ -1,28 +1,15 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import Header from "./header.jsx";
-import {Provider} from "react-redux";
-import configureStore from "redux-mock-store";
-import NameSpace from "../../reducer/name-space.js";
-import {AuthorizationStatus} from "../../reducer/user/user.js";
+import {Header} from "./header.jsx";
 
-const mockStore = configureStore([]);
 
 it(`Should Header render correctly`, () => {
 
-  const store = mockStore({
-    [NameSpace.USER]: {
-      authorizationStatus: AuthorizationStatus.NO_AUTH,
-    },
-  });
-
   const tree = renderer
     .create(
-        <Provider store={store}>
-          <Header
-            isMain={true}
-          />
-        </Provider>
+        <Header
+          isMain={true}
+        />
     ).toJSON();
 
   expect(tree).toMatchSnapshot();
--- src/components/sign-in/sign-in.test.js
+++ src/components/sign-in/sign-in.test.js
@@ -1,33 +1,16 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import SignIn from "./sign-in.jsx";
-import {Provider} from "react-redux";
-import configureStore from "redux-mock-store";
-import NameSpace from "../../reducer/name-space.js";
-import {AuthorizationStatus} from "../../reducer/user/user.js";
-
-const mockStore = configureStore([]);
-
 
 const noop = () => {};
 
 it(`SignIn component render correctly`, () => {
 
-
-  const store = mockStore({
-    [NameSpace.USER]: {
-      authorizationStatus: AuthorizationStatus.NO_AUTH,
-    },
-  });
-
-
   const tree = renderer
     .create(
-        <Provider store={store}>
-          <SignIn
-            onSubmit={noop}
-          />
-        </Provider>
+        <SignIn
+          onSubmit={noop}
+        />
     ).toJSON();
 
   expect(tree).toMatchSnapshot();
