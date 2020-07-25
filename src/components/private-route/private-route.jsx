1bcf3b9f75ea6f30b45e4f9a18816b503b8e299b
Georg <georgiy.nikitin@gmail.com>
2020-07-25 17:11:33 +0300

    Создает components/private-route

---
+++ src/components/private-route/private-route.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {Route, Redirect} from "react-router-dom";
+import {connect} from "react-redux";
+import {AppRoute} from "../../const.js";
+import {getAuthorizationStatus} from "../../reducer/user/selectors.js";
+
+
+const PrivateRoute = (props) => {
+  const {render, path, exact, isAuthorizationStatus} = props;
+
+  return (
+    <Route
+      path={path}
+      exact={exact}
+      render={() => {
+        return (
+          isAuthorizationStatus ? render() : <Redirect to={AppRoute.LOGIN} />
+        );
+      }}
+    />
+  );
+};
+
+PrivateRoute.propTypes = {
+  isAuthorizationStatus: PropTypes.bool.isRequired,
+  exact: PropTypes.bool.isRequired,
+  path: PropTypes.string.isRequired,
+  render: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  isAuthorizationStatus: getAuthorizationStatus(state),
+});
+
+
+export {PrivateRoute};
+export default connect(mapStateToProps)(PrivateRoute);
