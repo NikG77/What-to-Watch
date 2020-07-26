e0743887157e4321b8e800054bbd5dec4a16382f
Georg <georgiy.nikitin@gmail.com>
2020-07-26 13:49:03 +0300

    Добавляет компонент MyList

---
+++ src/components/my-list/my-list.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Logo from "../logo/logo.jsx";
+import Footer from "../footer/footer.jsx";
+import {getUserInfo} from "../../reducer/user/selectors.js";
+import {connect} from "react-redux";
+
+
+const MyList = (props) => {
+  const {userInfo} = props;
+
+  return (
+    <div className="user-page">
+      <header className="page-header user-page__head">
+        <Logo />
+
+        <h1 className="page-title user-page__title">My list</h1>
+
+        <div className="user-block">
+          <div className="user-block__avatar">
+            <img src={userInfo.avatarUrl} alt="User avatar" width="63" height="63" />
+          </div>
+        </div>
+      </header>
+
+      <section className="catalog">
+        <h2 className="catalog__title visually-hidden">Catalog</h2>
+
+        <div className="catalog__movies-list">
+          <article className="small-movie-card catalog__movies-card">
+            <div className="small-movie-card__image">
+              <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg" alt="Fantastic Beasts: The Crimes of Grindelwald" width="280" height="175" />
+            </div>
+            <h3 className="small-movie-card__title">
+              <a className="small-movie-card__link" href="movie-page.html">Fantastic Beasts: The Crimes of Grindelwald</a>
+            </h3>
+          </article>
+
+          <article className="small-movie-card catalog__movies-card">
+            <div className="small-movie-card__image">
+              <img src="img/bohemian-rhapsody.jpg" alt="Bohemian Rhapsody" width="280" height="175" />
+            </div>
+            <h3 className="small-movie-card__title">
+              <a className="small-movie-card__link" href="movie-page.html">Bohemian Rhapsody</a>
+            </h3>
+          </article>
+
+          <article className="small-movie-card catalog__movies-card">
+            <div className="small-movie-card__image">
+              <img src="img/macbeth.jpg" alt="Macbeth" width="280" height="175" />
+            </div>
+            <h3 className="small-movie-card__title">
+              <a className="small-movie-card__link" href="movie-page.html">Macbeth</a>
+            </h3>
+          </article>
+
+          <article className="small-movie-card catalog__movies-card">
+            <div className="small-movie-card__image">
+              <img src="img/aviator.jpg" alt="Aviator" width="280" height="175" />
+            </div>
+            <h3 className="small-movie-card__title">
+              <a className="small-movie-card__link" href="movie-page.html">Aviator</a>
+            </h3>
+          </article>
+
+
+          <article className="small-movie-card catalog__movies-card">
+            <div className="small-movie-card__image">
+              <img src="img/shutter-island.jpg" alt="Shutter Island" width="280" height="175" />
+            </div>
+            <h3 className="small-movie-card__title">
+              <a className="small-movie-card__link" href="movie-page.html">Shutter Island</a>
+            </h3>
+          </article>
+        </div>
+      </section>
+
+      <Footer />
+    </div>
+  );
+
+};
+
+MyList.propTypes = {
+  userInfo: PropTypes.oneOfType([
+    () => null,
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      email: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      avatarUrl: PropTypes.string.isRequired,
+    }).isRequired,
+  ]),
+};
+
+
+const mapStateToProps = (state) => ({
+  userInfo: getUserInfo(state),
+});
+
+export {MyList};
+export default connect(mapStateToProps)(MyList);
+
