cbcfe6e339f1bcb8f7e3d5eaa831e4bcf9afb6fc
Georg <georgiy.nikitin@gmail.com>
2020-07-29 12:28:46 +0300

    Добавляет переход на главную страницу по Logo

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import {Link} from "react-router-dom";
 import {AppRoute} from "../../const.js";
 import PropTypes from "prop-types";
+import {connect} from "react-redux";
+import {ActionCreator} from "../../reducer/watch/watch.js";
 
-const Logo = ({className}) => {
+const Logo = (props) => {
+  const {className, onLinkToMainPage} = props;
 
   return (
     <div className="logo">
-      <Link to={AppRoute.ROOT} className={`logo__link ${className || ``}`}>
+      <Link to={AppRoute.ROOT}
+        className={`logo__link ${className || ``}`}
+        onClick={onLinkToMainPage}
+      >
         <span className="logo__letter logo__letter--1">W</span>
         <span className="logo__letter logo__letter--2">T</span>
         <span className="logo__letter logo__letter--3">W</span>
@@ -17,9 +23,19 @@ const Logo = ({className}) => {
 };
 
 Logo.propTypes = {
-  className: PropTypes.string
+  className: PropTypes.string,
+  onLinkToMainPage: PropTypes.func.isRequired,
 };
 
-export default Logo;
+
+const mapDispatchToProps = (dispatch) => ({
+  onLinkToMainPage() {
+    dispatch(ActionCreator.setId(null));
+  }
+});
+
+export {Logo};
+
+export default connect(null, mapDispatchToProps)(Logo);
 
 
