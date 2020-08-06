b91c722c79b63eeead2d71d4cfec777ac30e26d6
Georg <georgiy.nikitin@gmail.com>
2020-08-06 15:01:53 +0300

    Обновит тесты

--- src/components/sign-in/sign-in.e2e.test.js
+++ src/components/sign-in/sign-in.e2e.test.js
@@ -3,7 +3,7 @@ import Enzyme, {mount} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import SignIn from "./sign-in";
 
-jest.mock(`../logo/logo.jsx`, () => `div`);
+jest.mock(`../logo/logo.tsx`, () => `div`);
 
 Enzyme.configure({
   adapter: new Adapter(),
